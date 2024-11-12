40bfac359be103434b8cf163b55a0a50f552a6db
alexsandrov16 <al3x5.dev@gmail.com>
2024-11-12 11:34:49 -0500

    Added support for flash message

---
+++ src/Session/Flash.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mk4U\Http\Session;
+
+use Leaf\Flash as LeafFlash;
+
+/**
+ * Flash class
+ */
+trait Flash
+{
+
+    #    [
+    #        '_mk4u_flash'=>[
+    #            '_new'=>[],
+    #            '_old'=>[]
+    #        ]
+    #    ];
+
+    private static function setFlash(string $name, $value): null
+    {
+        $_SESSION['_mk4u_flash']['_new'][] = $name;
+        self::set($name, $value);
+        return null;
+    }
+
+    private static function getFlash(string $name): ?string
+    {
+        $messages = $_SESSION['_mk4u_flash']['_new'] ?? [];
+        foreach ($messages as $key => $value) {
+            if ($value === $name) {
+                $_SESSION['_mk4u_flash']['_old'][] = $name;
+                $flash = self::get($value);
+                unset($_SESSION['_mk4u_flash']['_new'][$key]);
+                self::remove($value);
+                break;
+            }
+        }
+
+        return $flash ?? null;
+    }
+}
